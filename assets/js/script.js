99c45a44daa326eb210c66a8a54c2398e9b7d156
Muhammad Fahad Akram <muhammadsalman.webdev@gmail.com>
2024-11-24 16:37:28 +0500

    Home Page Desktop

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+const industryTabsBtns = document.querySelectorAll(
+  "section.industry-section[data-change-industry-tabs] .industry-container > .industry-cards > .card"
+);
+const industryTabs = document.querySelectorAll(
+  "section.industry-section[data-change-industry-tabs] .industry-container > .industry-cards-desc-container > .industry-desc"
+);
+industryTabsBtns.forEach((tabBtn, index) => {
+  tabBtn.addEventListener("click", () => {
+    industryTabsBtns.forEach((_tabBtn, _index) => {
+      const tab = industryTabs[_index];
+      if (index === _index) {
+        _tabBtn.classList.add("active");
+
+        tab.classList.add("show");
+        setTimeout(() => {
+          tab.classList.add("anim");
+        }, 10);
+      } else {
+        _tabBtn.classList.remove("active");
+        tab.classList.remove("anim");
+        setTimeout(() => {
+          tab.classList.remove("show");
+        }, 500);
+      }
+    });
+  });
+});
